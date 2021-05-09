14f2c20e5ffc6990c50a8740bec4dc6c00f1a1a6
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2021-05-09 10:10:58 +0900

    feat(): css,jsデレクトリ、ファイル作成

--- my-snow-monkey.php
+++ my-snow-monkey.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: My Snow Monkey
  * Description: このプラグインに、あなたの Snow Monkey 用カスタマイズコードを書いてください。
- * Version: 0.1.1
+ * Version: 0.2.1
  *
  * @package my-snow-monkey
  * @author inc2734
@@ -14,5 +14,43 @@
  */
 $theme = wp_get_theme( get_template() );
 if ( 'snow-monkey' !== $theme->template && 'snow-monkey/resources' !== $theme->template ) {
-	return;
+    return;
 }
+
+/**
+ * Directory url of this plugin
+ *
+ * @var string
+ */
+define( 'MY_SNOW_MONKEY_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
+
+/**
+ * Directory path of this plugin
+ *
+ * @var string
+ */
+define( 'MY_SNOW_MONKEY_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+
+// スタイルとスクリプトの読み込み
+add_action('wp_enqueue_scripts',
+    function () {
+        wp_enqueue_style('style', MY_SNOW_MONKEY_URL.'/css/style.css',array(),date_i18n( 'YmdHis'));
+        wp_enqueue_script('script', MY_SNOW_MONKEY_URL.'/js/main.js', array(), false, true);
+    },
+    11, 2);
+
+add_action('admin_enqueue_scripts',
+    function () {
+        wp_enqueue_style('admin_style', MY_SNOW_MONKEY_URL.'/css/style.css');
+    },
+    10, 2);
+
+// Google fonts 読み込み
+//add_action('wp_head',
+//    function () {
+//          $fonts = <<<EOF
+//              <link rel="preconnect" href="https://fonts.gstatic.com">
+//              <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=M+PLUS+Rounded+1c:wght@500;800&display=swap" rel="stylesheet">
+//              EOF;
+//          echo $fonts;
+//    });
